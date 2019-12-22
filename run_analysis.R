b68509113ebe5b050b0b109c16bf8bb06e64b9a1
egalvan91 <56476449+egalvan91@users.noreply.github.com>
2019-12-21 23:12:26 -0300

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -15,15 +15,15 @@ run_analysis <- function(){
     subject_test <- read.table(file.path(getwd(), "UCI HAR Dataset/test/subject_test.txt"), header = FALSE)
 
     # Joining information with activity
-    activity_test <- merge(y_test, activity_labels, by = "V1")[2]
+    activity_test <- activity_labels[y_test$V1, 2]
 
     # Rename columns
     names(x_test) <- features$V2
-    names(activity_test) <- "activity"
+    names(y_test) <- "activity"
     names(subject_test) <- "idsubject"
 
     # Joining all the test information
-    test <- cbind(subject_test, activity_test, x_test)
+    test <- cbind(subject_test, y_test, x_test)
 
     # Reading files x_train, y_train and subject_train
     x_train <- read.table(file.path(getwd(), "UCI HAR Dataset/train/X_train.txt"), header = FALSE)
@@ -31,15 +31,15 @@ run_analysis <- function(){
     subject_train <- read.table(file.path(getwd(), "UCI HAR Dataset/train/subject_train.txt"), header = FALSE)
 
     # Joining information with activity
-    activity_train <- merge(y_train, activity_labels, by = "V1")[2]
+    y_train$V1 <- activity_labels[y_train$V1, 2]
 
     # Rename columns
     names(x_train) <- features$V2
-    names(activity_train) <- "activity"
+    names(y_train) <- "activity"
     names(subject_train) <- "idsubject"
 
     # Joining all the train information
-    train <- cbind(subject_train, activity_train, x_train)
+    train <- cbind(subject_train, y_train, x_train)
 
     # Joining test and train information
     train_test <- rbind(test, train)
